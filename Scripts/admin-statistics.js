e13038e5b30b8cab248c0127b1285262774fec71
Kevin Breurken <kevinbreurken@gmail.com>
2020-10-12 16:17:46 +0200

    Added file structure for admin-statistics page

---
+++ Scripts/admin-statistics.js
@@ -0,0 +1,9 @@
+/**
+ * //TODO: Create dummy metrics for statistics
+ * //TODO: Create filter option for time (Day/Week/Month/Year/Total)
+ * 
+ * //TODO: Component - Number Display
+ * //TODO: Component - Pie chart Display
+ * //TODO: Component - Top 10 Display
+ * //TODO: Component - Pages list display
+ */
