32585d8006c94bddae5e08a3dae47d6ca68f9d9f
Kevin Breurken <kevinbreurken@gmail.com>
2020-10-18 19:21:27 +0200

    Added admin toggle option to the header

--- Scripts/general.js
+++ Scripts/general.js
@@ -1,17 +1,66 @@
 /**
- * Requires a webserver to be run.
+ * Check whether the page is currently accessed by a Admin Profile.
  */
-document.addEventListener('DOMContentLoaded', function () {
-    //add the general stylesheet to the header.
+var isOnAdminProfile;
+/**
+ * name of the current menu. ex: 'home/statistics'.
+ */
+var currentMenuType;
+
+$(document).ready(function () {
+    //add the general stylesheet to the page's header.
     $('head').append('<link rel="stylesheet" type="text/css" href="Content/general.css">');
 
-    //Add the header to the start of the body
+    //Add the header to the start of the body.
     var headerElement = document.createElement('header');
     document.body.insertBefore(headerElement, document.body.firstChild);
     $('header').load("Includes/general-header.html");
 
+    //When the header is ready...
+    $('header').ready(function () {
+        //Change the menu to admin if page is a admin page.
+        if (isOnAdminProfile)
+            overrideMenuButtons([["account", "#"], ["overview data", "#"], ["statistics", "#"]]);
+
+        //set the current active menu.
+        updateMenuButtons();
+    });
+
+    //Add the footer to the end of the body.
     var footerElement = document.createElement('footer');
     document.body.appendChild(footerElement);
     $('footer').load("Includes/general-footer.html");
 
-}, false);
+});
+
+/**
+ * Updates the button menu's 'current' attributes.
+ * @param {string} typeName name of the menu button's type attribute. 
+ */
+function updateMenuButtons() {
+    //Get every main menu button
+    $('.main-menu-buttons').each(function (i, obj) {
+        if (currentMenuType == $(this).attr("type")) {
+            $(this).attr("current", "");
+            return;
+        }
+    });
+}
+
+/**
+ * Deletes the current menu and changes to a new set.
+ * @param {array} newButtons data for the new buttons. 
+ */
+function overrideMenuButtons(newButtons) {
+    //remove all menu buttons.
+    const itemList = $('.main-menu-items');
+    itemList.empty();
+
+    for (let i = 0; i < newButtons.length; i++) {
+        //Add the homepage icon to the first item only.
+        const homeImageHTML = i == 0 ? '<img src="Image/home-icon.png">' : '';
+        //Create the list item.
+        $(itemList).append(`<li><a class="main-menu-buttons" href="${newButtons[i][1]}
+        " type="${newButtons[i][0]}">${homeImageHTML}${newButtons[i][0]}</a></li>`);
+    }
+}
