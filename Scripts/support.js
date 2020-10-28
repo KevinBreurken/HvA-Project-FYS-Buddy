f4e89a189c6fc18be96ebd823bc34173e9f2ee09
Barry Stavenuiter <barry.stavenuiter@hva.nl>
2020-10-28 22:15:00 +0100

    Added collapsible block elements for FAQ's

--- Scripts/support.js
+++ Scripts/support.js
@@ -1,3 +1,18 @@
+// Collapsible block elements:
+const coll = document.querySelectorAll(".collapsible");
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+// When clicking back button:
 document.querySelector(".support-controls button#back").addEventListener("click", function() {
     window.location.href = "index.html";
-})
+});
