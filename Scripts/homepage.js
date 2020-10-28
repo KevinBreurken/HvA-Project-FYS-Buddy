b17aa2e4fbb45e2367778168ae67d95a7396d961
Irene Doodeman <irene.d166@gmail.com>
2020-10-28 20:58:58 +0100

    added javascript to the favorite icons

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -1,5 +1,7 @@
+//clicks on the 'All results' tab so it's open by default
 document.getElementById("default-active").click();
 
+//function to switch the tab and active tab-button
 function openTabContent (tabContentName, thisKeyword) {
     var tabButton = document.getElementsByClassName("tab-button");
     var tabContent = document.getElementsByClassName("tab-content");
@@ -15,14 +17,30 @@ function openTabContent (tabContentName, thisKeyword) {
     thisKeyword.style.backgroundColor = "#c11905";
 }
 
+//displays the overlay and the overlay background
 function displayOverlay (overlayId) {
     document.getElementById(overlayId).style.display = "flex";
     var overlayBackground = document.getElementById("overlay-background")
     overlayBackground.style.display = "block";
 }
 
+//function to close user displays or overlays
 function closeFunction (currentDisplay) {
     document.getElementById(currentDisplay).style.display = "none";
     var overlayBackground = document.getElementById("overlay-background")
     overlayBackground.style.display = "none";
+}
+
+//function to swap the favorites icon
+function swapFavoritesIcon (currentIconId, newIconId) {
+    var currentIcon = document.getElementById(currentIconId);
+    var newIcon = document.getElementById(newIconId);
+    currentIcon.style.display = "none";
+    newIcon.style.display = "";
+}
+
+//function for prototype
+function favoritesRemove (currentUserDisplay) {
+    var displayToRemove = document.getElementById(currentUserDisplay)
+    displayToRemove.style.display = "none";
 }
