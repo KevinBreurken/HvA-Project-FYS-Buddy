0474ec05b017f2e4744f6b5a0691d6d0b3af1788
Kevin Breurken <kevinbreurken@gmail.com>
2020-11-12 19:36:17 +0100

    modified project structure
    placed all images in the Content/Images folder

--- Scripts/general.js
+++ Scripts/general.js
@@ -24,12 +24,12 @@ $(document).ready(function () {
     //Add the header to the start of the body.
     var headerElement = document.createElement('header');
     document.body.insertBefore(headerElement, document.body.firstChild);
-    $('header').load("Includes/general-header.html", onHeaderLoaded);
+    $('header').load("general-header.html", onHeaderLoaded);
 
     //Add the footer to the end of the body.
     var footerElement = document.createElement('footer');
     document.body.appendChild(footerElement);
-    $('footer').load("Includes/general-footer.html");
+    $('footer').load("general-footer.html");
 });
 
 function onHeaderLoaded() {
@@ -98,7 +98,7 @@ function overrideMenuButtons(newButtons) {
     for (let i = 0; i < newButtons.length; i++) {
         //Add the homepage icon to the first item only.
         const homeImageHTML = i == 0 ? `<img class="main-menu-home-icon"
-        src="Image/home-icon.png">`: '';
+        src="Content/Images/home-icon.png">`: '';
         //Create the list item.
         $(itemList).append(`<li><a class="main-menu-buttons" href="${newButtons[i][1]}
         " type="${newButtons[i][0]}">${homeImageHTML}${newButtons[i][0]}</a></li>`);
