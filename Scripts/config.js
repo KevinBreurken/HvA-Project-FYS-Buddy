2971cbaff3d338c4c7991cdea482284f837f3e39
Kevin Breurken <kevinbreurken@gmail.com>
2020-11-20 23:28:43 +0100

    added config.js

---
+++ Scripts/config.js
@@ -0,0 +1,6 @@
+FYSCloud.API.configure({
+    url: "https://api.fys.cloud",
+    apiKey: "fys_is111_1.14Sh6xypTzeSUHD4",
+    database: "fys_is111_1_kevintest", //Change this to the database-name you want to use for testing.
+    environment: "dev"
+});
