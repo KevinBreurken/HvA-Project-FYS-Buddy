32c5707e2cc799e7d7a283e0828423cd4e96c729
kiet van wijk <kiet.van.wijk@hva.nl>
2020-11-24 17:02:51 +0100

    Changed admin-panel to be dynamic

--- Scripts/Pages/admin-panel.js
+++ Scripts/Pages/admin-panel.js
@@ -17,33 +17,32 @@ function deleteRow(number) {
     overlay.remove()
 }
 
+// Temporary dummy database connection - will be removed once moved out of development
+FYSCloud.API.configure({
+    url: "https://api.fys.cloud",
+    apiKey: "fys_is111_1.14Sh6xypTzeSUHD4",
+    database: "fys_is111_1_dev_kiet",
+    environment: "dev"
+});
+
 FYSCloud.API.queryDatabase(
     "SELECT * FROM user"
 ).done(function (data) {
+    var tdArray = [];
+    var cellArray = [];
+
     for (let i = 0; i < data.length; i++) {
-        var tR = document.createElement("TR");
-        tR.setAttribute("id", "myTr-" + i);
-        document.getElementById("myTable").appendChild(tR);
+        var tr = document.createElement("TR");
+        tr.setAttribute("id", "Tr-" + i);
+        document.getElementById("tableBody").appendChild(tr);
 
-        var tD1 = document.createElement("TD");
-        var tD2 = document.createElement("TD");
-        var tD3 = document.createElement("TD");
-        var tD4 = document.createElement("TD");
-
-        var cell1 = document.createTextNode(data[i]['voornaam']);
-        var cell2 = document.createTextNode(data[i]['achternaam']);
-        var cell3 = document.createTextNode(data[i]['email']);
-        var cell4 = document.createTextNode(data[i]['age']);
-
-        tD1.appendChild(cell1);
-        tD2.appendChild(cell2);
-        tD3.appendChild(cell3);
-        tD4.appendChild(cell4);
-
-        document.getElementById("myTr-" + i).appendChild(tD1);
-        document.getElementById("myTr-" + i).appendChild(tD2);
-        document.getElementById("myTr-" + i).appendChild(tD3);
-        document.getElementById("myTr-" + i).appendChild(tD4);
+        // Create element <td> for each object property
+        for (let j = 0; j < Object.keys(data[1]).length; j++) {
+            tdArray[j] = document.createElement("TD");
+            cellArray[j] = document.createTextNode(data[i][Object.keys(data[0])[j]]);
+            tdArray[j].appendChild(cellArray[j]);
+            document.getElementById("Tr-" + i).appendChild(tdArray[j]);
+        }
     }
 }).fail(function (reason) {
     console.log(reason);
