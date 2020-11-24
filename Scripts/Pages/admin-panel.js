f396f0debf41e6839399b915de0f6a01d40dc457
kiet van wijk <kiet.van.wijk@hva.nl>
2020-11-24 15:47:17 +0100

    Laid the foundation for admin-panel database integration

--- Scripts/Pages/admin-panel.js
+++ Scripts/Pages/admin-panel.js
@@ -16,3 +16,35 @@ function deleteRow(number) {
     let overlay = document.getElementById('row-data-' + number)
     overlay.remove()
 }
+
+FYSCloud.API.queryDatabase(
+    "SELECT * FROM user"
+).done(function (data) {
+    for (let i = 0; i < data.length; i++) {
+        var tR = document.createElement("TR");
+        tR.setAttribute("id", "myTr-" + i);
+        document.getElementById("myTable").appendChild(tR);
+
+        var tD1 = document.createElement("TD");
+        var tD2 = document.createElement("TD");
+        var tD3 = document.createElement("TD");
+        var tD4 = document.createElement("TD");
+
+        var cell1 = document.createTextNode(data[i]['voornaam']);
+        var cell2 = document.createTextNode(data[i]['achternaam']);
+        var cell3 = document.createTextNode(data[i]['email']);
+        var cell4 = document.createTextNode(data[i]['age']);
+
+        tD1.appendChild(cell1);
+        tD2.appendChild(cell2);
+        tD3.appendChild(cell3);
+        tD4.appendChild(cell4);
+
+        document.getElementById("myTr-" + i).appendChild(tD1);
+        document.getElementById("myTr-" + i).appendChild(tD2);
+        document.getElementById("myTr-" + i).appendChild(tD3);
+        document.getElementById("myTr-" + i).appendChild(tD4);
+    }
+}).fail(function (reason) {
+    console.log(reason);
+});
--- Scripts/config.js
+++ Scripts/config.js
@@ -1,6 +1,6 @@
 FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is111_1.14Sh6xypTzeSUHD4",
-    database: "fys_is111_1_dev", //Change this to the database-name you want to use for testing.
+    database: "fys_is111_1_dev_kiet", //Change this to the database-name you want to use for testing.
     environment: "dev"
 });
