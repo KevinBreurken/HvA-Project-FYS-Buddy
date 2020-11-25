ffce2bdd49f0bf2b8e02a9a7f80a697b2d0dc3a8
Kevin Breurken <kevinbreurken@gmail.com>
2020-11-25 14:56:56 +0100

    added matches category querys

--- Scripts/Pages/admin-querys.js
+++ Scripts/Pages/admin-querys.js
@@ -1,4 +1,3 @@
-
 //----  TRAFIC ----
 // ** SITE VISITORS **
 
@@ -13,59 +12,14 @@
 // ** TOTAL ACCOUNTS **
 FYSCloud.API.queryDatabase(
     "SELECT * FROM users"
-).done(function(data) {
+).done(function (data) {
     $('#total-accounts').html(data.length);
-}).fail(function(reason) {
+    fillMatchesWindow(data.length);
+}).fail(function (reason) {
     console.log(reason);
 });
 
 // ** AVERAGE VISIT TIME **
-
-
-//----  MATCHING  ----
-// ** MADE MATCHES **
-
-// ** CONTACTS SHARED **
-
-// ** AVERAGE MATCHES PER USER **
-
-// ** AVERAGE CONTACTS SHARED PER USER **
-
-// ** MOST MATCHES WITH EQUAL INTERESTS **
-FYSCloud.API.queryDatabase(
-    "SELECT * " +
-    "FROM interests " +
-    "ORDER BY matchAmount " +
-    "DESC LIMIT 10;"
-).done(function(data) {
-    let listData = [[data.length],[data.length]];
-    for (let i = 0; i < data.length; i++) {
-        listData[0][i] = data[i].interestName;
-        listData[1][i] = data[i].matchAmount;
-    }
-    $('#most-match-equal-interests').html(makeOL(listData));
-}).fail(function(reason) {
-    console.log(reason);
-    $('#most-match-equal-interests').html("ERROR");
-});
-
-// ** MOST MATCHES WITH EQUAL DESTINATION **
-FYSCloud.API.queryDatabase(
-    "SELECT * " +
-    "FROM destinations " +
-    "ORDER BY matchAmount " +
-    "DESC LIMIT 10;"
-).done(function(data) {
-    let listData = [[data.length],[data.length]];
-    for (let i = 0; i < data.length; i++) {
-        listData[0][i] = data[i].destinationName;
-        listData[1][i] = data[i].matchAmount;
-    }
-    $('#most-match-equal-destination').html(makeOL(listData));
-}).fail(function(reason) {
-    console.log(reason);
-    $('#most-match-equal-destination').html("ERROR");
-});
 
 //----  PAGES  ----
 // ** BOUNCE RATE **
@@ -73,14 +27,14 @@ FYSCloud.API.queryDatabase(
     "SELECT * " +
     "FROM pages " +
     "ORDER BY bounce DESC;"
-).done(function(data) {
-    let listData = [[data.length],[data.length]];
+).done(function (data) {
+    let listData = [[data.length], [data.length]];
     for (let i = 0; i < data.length; i++) {
         listData[0][i] = data[i].pageName;
         listData[1][i] = data[i].bounce;
     }
     $('#page-bounce').html(makeOL(listData));
-}).fail(function(reason) {
+}).fail(function (reason) {
     console.log(reason);
     $('#page-bounce').html("ERROR");
 });
@@ -90,14 +44,75 @@ FYSCloud.API.queryDatabase(
     "SELECT * " +
     "FROM pages " +
     "ORDER BY visits DESC;"
-).done(function(data) {
-    let listData = [[data.length],[data.length]];
+).done(function (data) {
+    let listData = [[data.length], [data.length]];
     for (let i = 0; i < data.length; i++) {
         listData[0][i] = data[i].pageName;
         listData[1][i] = data[i].visits;
     }
     $('#page-views').html(makeOL(listData));
-}).fail(function(reason) {
+}).fail(function (reason) {
     console.log(reason);
     $('#page-views').html("ERROR");
 });
+
+function fillMatchesWindow(totalUserCount) {
+    //----  MATCHING  ----
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM matches"
+    ).done(function (data) {
+        let totalMatchesCount = data.length;
+        let totalContactsShared = 0;
+        for (let i = 0; i < data.length; i++)
+            totalContactsShared += data[i].contactShared;
+
+        console.log(totalUserCount);
+
+        // ** MADE MATCHES **
+        $('#made-matches').html(totalMatchesCount);
+        // ** AVERAGE MATCHES PER USER
+        $('#made-match-average').html(totalUserCount / totalMatchesCount);
+        // ** CONTACT SHARED **
+        $('#contacts-shared').html(totalContactsShared);
+        // ** AVERAGE CONTACTS SHARED PER USER **
+        $('#contacts-shared-average').html((totalUserCount / totalContactsShared).toFixed(2));
+    }).fail(function (reason) {
+        console.log(reason);
+    });
+
+    // ** MOST MATCHES WITH EQUAL INTERESTS **
+    FYSCloud.API.queryDatabase(
+        "SELECT * " +
+        "FROM interests " +
+        "ORDER BY matchAmount " +
+        "DESC LIMIT 10;"
+    ).done(function (data) {
+        let listData = [[data.length], [data.length]];
+        for (let i = 0; i < data.length; i++) {
+            listData[0][i] = data[i].interestName;
+            listData[1][i] = data[i].matchAmount;
+        }
+        $('#most-match-equal-interests').html(makeOL(listData));
+    }).fail(function (reason) {
+        console.log(reason);
+        $('#most-match-equal-interests').html("ERROR");
+    });
+
+// ** MOST MATCHES WITH EQUAL DESTINATION **
+    FYSCloud.API.queryDatabase(
+        "SELECT * " +
+        "FROM destinations " +
+        "ORDER BY matchAmount " +
+        "DESC LIMIT 10;"
+    ).done(function (data) {
+        let listData = [[data.length], [data.length]];
+        for (let i = 0; i < data.length; i++) {
+            listData[0][i] = data[i].destinationName;
+            listData[1][i] = data[i].matchAmount;
+        }
+        $('#most-match-equal-destination').html(makeOL(listData));
+    }).fail(function (reason) {
+        console.log(reason);
+        $('#most-match-equal-destination').html("ERROR");
+    });
+}
