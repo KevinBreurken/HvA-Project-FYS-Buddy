4d88d6042b9a0cb98e8e6096d54f18ef1a8c165f
kiet van wijk <kiet.van.wijk@hva.nl>
2020-11-29 00:56:06 +0100

    Addition of buttons to admin-panel
    Set the groundwork for buttons(not functional atm) and edited css to match the new button attributes

--- Scripts/Pages/admin-panel.js
+++ Scripts/Pages/admin-panel.js
@@ -1,20 +1,8 @@
-function showDetails() {
-    let overlay = document.getElementById('overlay')
-    let overlayBackground = document.getElementById('overlay-background')
-    overlay.style.display = 'block'
-    overlayBackground.style.display = 'block'
-}
-
 function closeDetails() {
     let overlay = document.getElementById('overlay')
     let overlayBackground = document.getElementById('overlay-background')
     overlay.style.display = 'none'
     overlayBackground.style.display = 'none'
-}
-
-function deleteRow(number) {
-    let overlay = document.getElementById('row-data-' + number)
-    overlay.remove()
 }
 
 // Temporary dummy database connection - will be removed once moved out of development
@@ -28,23 +16,45 @@ FYSCloud.API.configure({
 FYSCloud.API.queryDatabase(
     "SELECT * FROM user"
 ).done(function (data) {
-    var tdArray = [];
-    var cellArray = [];
+    var tdArray = []
+    var cellArray = []
 
-    // Create a <tr> element for each property in the object
-    for (let i = 0; i < data.length; i++) {
-        var tr = document.createElement("TR");
-        tr.setAttribute("id", "Tr-" + i);
-        document.getElementById("tableBody").appendChild(tr);
+    // TODO Unsure about using standard variables(i, j) for the for loop or row. column
+    // Create a row element for each property in the object
+    for (let row = 0; row < data.length; row++) {
+        var tr = document.createElement("tr")
 
-        // Create a <td> element for each attribute in the object
-        for (let j = 0; j < Object.keys(data[0]).length; j++) {
-            tdArray[j] = document.createElement("TD");
-            cellArray[j] = document.createTextNode(data[i][Object.keys(data[0])[j]]);
-            tdArray[j].appendChild(cellArray[j]);
-            document.getElementById("Tr-" + i).appendChild(tdArray[j]);
+        tr.setAttribute("id", "tr-" + row)
+        document.getElementById("tableBody").appendChild(tr)
+
+        // Create a column element for each attribute in the object
+        for (let column = 0; column < Object.keys(data[0]).length; column++) {
+            tdArray[column] = document.createElement("td")
+            cellArray[column] = document.createTextNode(data[row][Object.keys(data[0])[column]])
+            tdArray[column].appendChild(cellArray[column])
+            document.getElementById("tr-" + row).appendChild(tdArray[column])
+        }
+        // Add two columns to each rows for admin buttons
+        for (let column = 0; column < 2; column++) {
+            var buttonTd = document.createElement("td")
+
+            buttonTd.setAttribute("id", "td-" + row + "-" + column)
+            document.getElementById("tr-" + row).appendChild(buttonTd)
+
+            // First button is for deletion and second button for edit
+            var adminButton = document.createElement("button")
+
+            if (column === 0) {
+                adminButton.innerHTML = "Delete"
+                document.getElementById("td-" + row + "-" + column).appendChild(adminButton)
+            } else {
+                adminButton.innerHTML = "Edit"
+                document.getElementById("td-" + row + "-" + column).appendChild(adminButton)
+            }
         }
     }
 }).fail(function (reason) {
-    console.log(reason);
-});
+    console.log(reason)
+})
+
+
