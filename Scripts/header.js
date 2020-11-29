b8c6b1254d1d0503ad92d4fe6be64b6eb5ea3538
Kevin Breurken <kevinbreurken@gmail.com>
2020-11-29 21:39:10 +0100

    added close button to header
    also test version of loading the notifications from the FYSCloud

--- Scripts/header.js
+++ Scripts/header.js
@@ -23,13 +23,6 @@ function onHeaderLoaded() {
     const preText = 'Welcome, ';
     $('.profile-display-text').html(preText + testNames[Math.floor(Math.random() * testNames.length)]);
 
-    const notificationText = " has sent a contact request";
-    // $('.notification-text').html(testNames[Math.floor(Math.random() * testNames.length)] + notificationText);
-
-    $('.notification-text').each(function (index, element) {
-        $(this).html(testNames[Math.floor(Math.random() * testNames.length)] + notificationText);
-    });
-
 }
 
 /**
@@ -48,7 +41,7 @@ function updateMenuButtons() {
 
 function setNavigationVisibility(state) {
     $('.main-menu-buttons').toggle(state);
-    if(state === false)
+    if (state === false)
         $('.main-menu').empty();
 }
 
@@ -64,7 +57,7 @@ function overrideMenuButtons(newButtons) {
     for (let i = 0; i < newButtons.length; i++) {
         //Add the homepage icon to the first item only.
         const homeImageHTML = i == 0 ? `<img class="main-menu-home-icon"
-        src="Content/Images/home-icon.png">`: '';
+        src="Content/Images/home-icon.png">` : '';
         //Create the list item.
         $(itemList).append(`<li><a class="main-menu-buttons" href="${newButtons[i][1]}
         " type="${newButtons[i][0]}">${homeImageHTML}${newButtons[i][0]}</a></li>`);
@@ -73,6 +66,61 @@ function overrideMenuButtons(newButtons) {
     updateMenuButtons();
 }
 
-$(function (){
+$(function () {
     onHeaderLoaded();
 });
+
+//Fetch user notifications.
+FYSCloud.API.queryDatabase(
+    "SELECT * FROM notifications WHERE userID = ?",
+    [1] //TODO: add the logged in user ID to fetch it's own notifications.
+).done(function (notificationData) {
+
+    let notificationIDs = [notificationData.length];
+    for (let i = 0; i < notificationData.length; i++) {
+        notificationIDs[i] = notificationData[i]["sentUserID"];
+    }
+
+    let string = "(" + notificationIDs.toString() + ")";
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM user WHERE userID IN " + string
+    ).done(function (userData) {
+        let amountDisplayString = (userData.length > 9) ? "9+" : userData.length;
+        $("#notification-display-counter-text").html(amountDisplayString);
+
+        for (let i = 0; i < userData.length; i++) {
+            // $("#notification-display-list").append(addNotification(userData[i]));
+        }
+        console.log(userData);
+    }).fail(function (reason) {
+        console.log(reason);
+    });
+
+}).fail(function (reason) {
+    console.log(reason);
+});
+
+function addNotification(userData) {
+
+    let userID = userData["userID"];
+    //TODO: Change notification language.
+    let notificationText = userData["firstName"] + " heeft een contactverzoek gestuurd.";
+    return "<li class=\"notification-display-content-item\" id='notification-user-"+userID+"'>" +
+        "<div class=\"notification-text\">" + notificationText + "</div>" +
+        "<div class=\"notification-buttons\">" +
+        "<img class=\"notification-profile-icon\" src=\"Content/Images/open-profile.svg\" onclick=\"openProfile(" + userID + ")\">" +
+        "<img class=\"notification-close-icon\" src=\"Content/Images/close.svg\" onclick=\"closeNotification(" + userID + ")\">" +
+        "</div>" +
+        "</li>";
+}
+
+function openProfile(userID) {
+    //TODO:Open profile to correct profile.
+    window.open("profile2.html","_self");
+}
+
+function closeNotification(userID) {
+    //Remove the html element.
+    $("#notification-user-"+userID).remove();
+    //TODO:Remove database entry of notification.
+}
