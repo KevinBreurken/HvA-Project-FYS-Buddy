ce5409c1199ef5728bb42c163a228011576a4b72
Dylan <dylan.van.berg3@hva.nl>
2020-12-01 20:20:11 +0100

    first commit i need to fix another branch

---
+++ Scripts/Pages/index-page.js
@@ -0,0 +1,25 @@
+let users;
+
+function validationCheck() {
+    var emailInput = document.getElementById("e-mail").value;
+    var passwordInput = document.getElementById("password").value;
+
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM user"
+    ).done(function(data) {
+        for (let i = 0; i < data.length; i++) {
+            if(emailInput == data[i].email && passwordInput == data[i].password) {
+                loginUser(data[i].userID)
+                console.log(data[i].userName + ' is logged in!')
+            }else {
+                console.log("incorrect email or password")
+                return false;
+            }
+        }
+    })
+}
+function loginUser(id) {
+    FYSCloud.URL.redirect("profile.html", {
+        id: id
+    });
+}
