5ad53e1daef6e736d66117faa36caa095088a305
hannatoenbreker <toenbrh@hva.nl>
2020-12-01 18:18:26 +0100

    database practice

--- Scripts/Pages/profile.js
+++ Scripts/Pages/profile.js
@@ -31,8 +31,8 @@ if (month < 10) {
     month = '0' + month
 }
 ageCheck = year + '-' + month + '-' + day;
-document.getElementById("dateOfBirth").setAttribute("max", today);
-document.getElementById("dateOfBirth").setAttribute("min", ageCheck);
+//document.getElementById("dateOfBirth").setAttribute("max", today);
+//document.getElementById("dateOfBirth").setAttribute("min", ageCheck);
 
 let count;
 function countCharacters() {
@@ -43,6 +43,34 @@ function countCharacters() {
     countRemaining.textContent = counter;
 }
 count = document.getElementById('biography');
-count.addEventListener('keyup', countCharacters, false);
+//count.addEventListener('keyup', countCharacters, false);
 
+$(function () {
+    main();
+})
 
+function main() {
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM user"
+    ).done(function (data) {
+        console.log(data);
+        let template = $("#databases").html();
+
+        for (let i = 0; i < data.length; i++) {
+            let databases = $(template);
+            let databases2 = data[i];
+
+            //databases.find(".gebruikersnaam").text(databases.username)
+            $(".databases2").append(databases);
+        }
+    }).fail(function () {
+        alert("paniek");
+    });
+}
+// FYSCloud.API.queryDatabase(
+//     "SELECT * FROM user"
+// ).done(function(data) {
+//     console.log(data);
+// }).fail(function(reason) {
+//     console.log(reason);
+// });
