bef3914c940e1123500834f4a42862d9078c3389
Irene Doodeman <irene.d166@gmail.com>
2020-12-02 19:30:55 +0100

    implemented user data into the user-overlay

--- Scripts/Pages/homepage.js
+++ Scripts/Pages/homepage.js
@@ -27,48 +27,42 @@ function generateUserDisplays(tab, data) {
     let userDisplays = [];
     for (let i = 0; i < data.length; i++) {
 
-        userDisplays[i] = document.createElement("div");
-        userDisplays[i].className = "user-display";
-        userDisplays[i].setAttribute("id", "user-display-" + i);
+        console.log(data[i])
 
-        tab.append(userDisplays[i]);
-
-        //getting data
-        let userData = data[i];
-
-        //username
-        let username = userData.username == null ? "username" : userData.username;
-
-        //profile pic
-        let userUrl = userData.url;
-
-        //location
-        let location = userData.location == null ? "City, Country" : userData.location;
+        let userId = data[i].userId;  //userId
+        let username = data[i].username == null ? "username" : data[i].username;  //username
+        let userUrl = data[i].url;  //profile pic
+        let location = data[i].location == null ? "City, Country" : data[i].location; //location
 
         //start date
-        let date = new Date(userData.startDate);
-        let startDate = userData.startDate == null
+        let date = new Date(data[i].startDate);
+        let startDate = data[i].startDate == null
             ? " " : `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 
         //end date
-        date = new Date(userData.endDate);
-        let endDate = userData.endDate == null
+        date = new Date(data[i].endDate);
+        let endDate = data[i].endDate == null
             ? " " : `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 
         //type of buddy
         let buddy;
-        if (userData.travelBuddy === 1 && userData.activityBuddy === 1) {
+        if (data[i].travelBuddy === 1 && data[i].activityBuddy === 1) {
             buddy = "a buddy";
-        } else if (userData.travelBuddy === 1 && !(userData.activityBuddy === 1)) {
+        } else if (data[i].travelBuddy === 1 && !(data[i].activityBuddy === 1)) {
             buddy = "a travel buddy";
-        } else if (!(userData.travelBuddy === 1) && userData.activityBuddy === 1) {
+        } else if (!(data[i].travelBuddy === 1) && data[i].activityBuddy === 1) {
             buddy = "an activity buddy";
         } else {
             buddy = " a buddy";
         }
 
+        userDisplays[i] = document.createElement("div");
+        userDisplays[i].className = "user-display";
+        userDisplays[i].setAttribute("id", "user-display-" + userId);
+        tab.append(userDisplays[i]);
+
         userDisplays[i].innerHTML =
-            `<h1 id=user-display-h1-${i}>${username}</h1>
+            `<h1 id=user-display-h1-${userId}>${username}</h1>
             <img class="profile-picture" src="${userUrl}">
             <div>
             <p>${location}</p>
@@ -77,7 +71,7 @@ function generateUserDisplays(tab, data) {
             <p>${buddy}</p>
             </div>
             <div class="tab-content-column-4">
-            <button id="button1-${i}" onclick="displayOverlay('overlay')">more info</button>
+            <button id="button1-${i}" onclick="displayOverlay('${userId}')">more info</button>
             <button id="button2-${i}" onclick="closeFunction('user-display-${i}')">X</button>
             <div id="favorite-v1-${i}" onclick="swapFavoritesIcon('favorite-v1-${i}','favorite-v2-${i}')">
             <img class="favorite-icon" src="Content/Images/favorite-v1.png">
@@ -90,11 +84,24 @@ function generateUserDisplays(tab, data) {
     }
 }
 
-
 //displays the current overlay and the overlay-background
-function displayOverlay (overlayId) {
-    document.getElementById(overlayId).style.display = "flex";
-    document.getElementById("overlay-background").style.display = "block";
+function displayOverlay (currentUserId) {
+    FYSCloud.API.queryDatabase(
+        "SELECT * FROM user WHERE userId = ?", [currentUserId]
+    ).done(function(data) {
+        //todo: add placeholder
+        $("#overlay-row-1").html(`<img src="${data[0].url}">`);
+        $("#overlay-full-name").html(data[0].firstName + " " + data[0].lastName);
+        $("#overlay-username").html("a.k.a. " + data[0].username);
+        //todo: fix interests
+        $("#overlay-interests").html(data[0].interest);
+        $("#overlay-bio").html(data[0].bio);
+
+        document.getElementById("overlay").style.display = "flex";
+        document.getElementById("overlay-background").style.display = "block";
+    }).fail(function(reason) {
+        console.log(reason);
+    });
 }
 
 //function to close the active user-display or overlay
