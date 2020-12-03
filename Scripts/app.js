8591a81b3c67f254577fbdfd8f8c2e5aabac9ed5
Kevin Breurken <kevinbreurken@gmail.com>
2020-12-03 23:48:33 +0100

    added default css to all pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -16,7 +16,7 @@ if (appElement !== undefined) {
 
 let headElement = $('head');
 //add the general stylesheet to the page's header.
-headElement.append('<link rel="stylesheet" type="text/css" href="Content/CSS/default.css">');
+// headElement.append('<link rel="stylesheet" type="text/css" href="Content/CSS/default.css">');
 //add the favicon to the page's header.
 headElement.append(`<link rel='shortcut icon' type='image/x-icon' href='Content/Images/favicon.ico'/>`);
 //add the config file
