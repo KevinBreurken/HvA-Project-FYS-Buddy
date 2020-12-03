75f5c2b75e44205ed13703c308fea192cdbad0ef
H.R. Toenbreker <hanna.toenbreker@hva.nl>
2020-12-03 12:59:43 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- Scripts/config.js
+++ Scripts/config.js
@@ -2,5 +2,5 @@ FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is111_1.14Sh6xypTzeSUHD4",
     database: "fys_is111_1", //Change this to the database-name you want to use for testing.
-    environment: "mockup"
+    environment: "dev"
 });
