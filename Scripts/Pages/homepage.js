1e520f4731b752f62316f663a17df9f94ac63002
Irene Doodeman <irene.d166@gmail.com>
2020-12-04 22:42:53 +0100

    started cleaning up homepage code

--- Scripts/Pages/homepage.js
+++ Scripts/Pages/homepage.js
@@ -4,98 +4,89 @@ window.addEventListener('load', function () {
 })
 
 //todo: create different query's;
-//1.All results: all results matching the users location, date and gender preference
-//2.Friends
-//3.Friend requests (ingoing)
-//4.Favorites
-//filters
+//1.1 All results: all results matching the users location, date and gender preference
+//1.2 Friends
+//1.3 Friend requests (ingoing)
+//1.4 Favorites
+
+//2.1 match only gender
+//2.2 filters
 
 //todo: set button color depending on if there is an outgoing friend request, the user if friends with the user or no action
 //todo: send notification to the other user
 //function(s) for setting the status of the 'send friend request' button and sets database data
 
-//todo: set favorites data in the database when clicking
+//todo: set favorites data in the database when clicking (swap)
 
-//function to switch the tab and active tab-button
-function openTabContent (currentButton) {
+/** function to switch the tab and active tab-button */
+async function openTabContent (currentButton) {
     //swaps the button colors
     $(".tab-button").css("backgroundColor", "");
     $(currentButton).css("backgroundColor", "#c11905");
 
-    //const userList = getAllUsers();
-    //promise
+    let userList = await getUserData();
+    console.log(userList[0])
 
-    // for (let i = 0; i < userList.length; i++) {
-    //    tab.append(displayUser(userList[i]));
-    // }
 
-    //displayUser() - returns a string with all the elements and data for one userDisplay
+    $("#tab").html("");
+    //todo: use forEach()
+    for (let i = 0; i < userList.length; i++) {
+        //displayUser() returns a string with all the elements and data for one userDisplay
+       $("#tab").append(generateUserDisplay(userList[i]));
+    }
+}
 
-    FYSCloud.API.queryDatabase(
-        "SELECT * FROM user"
-    ).done(function(data) {
-        $("#tab").html("");
-        let userDisplays = [];
-        for (let i = 0; i < data.length; i++) {
+/** function for getting user data from the database */
+function getUserData() {
+    return new Promise(userList => {
+        FYSCloud.API.queryDatabase(
+            "SELECT * FROM user"
+        ).done(function(data) {
+            userList(data);
+        }).fail(function(reason) {
+            console.log(reason);
+        });
+    });
+}
 
-            const currentDisplayUserId = data[i].userId;  //userId
-            const username = data[i].username === "" ? "username" : data[i].username;  //username
-            const userUrl = data[i].url === "" ? "https://dev-is111-1.fys.cloud/uploads/profile-pictures/default-profile-picture.png" : data[i].url;  //profile pic
-            const location = data[i].location === "" ? "City, Country" : data[i].location; //location
+/** function for generating a user display */
+function generateUserDisplay(currentUser) {
+    let userId = currentUser.userId;
 
-            //start date
-            let date = new Date(data[i].startDate);
-            const startDate = data[i].startDate === "" ? " " : `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    let userDisplay = document.createElement("div");
+    userDisplay.className = "user-display";
+    userDisplay.setAttribute("id", "user-display-" + userId);
 
-            //end date
-            date = new Date(data[i].endDate);
-            const endDate = data[i].endDate === "" ? " " : `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    //todo date
+    //todo buddy
+    let buddy;
 
-            //type of buddy
-            let buddy;
-            if (data[i].travelBuddy === 1 && data[i].activityBuddy === 1) {
-                buddy = "a buddy";
-            } else if (data[i].travelBuddy === 1 && !(data[i].activityBuddy === 1)) {
-                buddy = "a travel buddy";
-            } else if (!(data[i].travelBuddy === 1) && data[i].activityBuddy === 1) {
-                buddy = "an activity buddy";
-            } else {
-                buddy = " a buddy";
-            }
-
-            userDisplays[i] = document.createElement("div");
-            userDisplays[i].className = "user-display";
-            userDisplays[i].setAttribute("id", "user-display-" + currentDisplayUserId);
-            $("#tab").append(userDisplays[i]);
-
-            userDisplays[i].innerHTML =
-                `<h1 id=user-display-h1-${currentDisplayUserId}>${username}</h1>
-            <img class="profile-picture" src="${userUrl}">
+    userDisplay.innerHTML =
+        `<h1 id=user-display-h1-${userId}>${currentUser.username}</h1>
+            <img class="profile-picture" src="${currentUser.url}">
             <div>
-            <p>${location}</p>
-            <p>from ${startDate}</p>
-            <p>until ${endDate}</p>
-            <p>${buddy}</p>
+            <p>${currentUser.location}</p>
+            <p>from ${currentUser.startDate}</p>
+            <p>until ${currentUser.endDate}</p>
+            <p>${"buddy"}</p>
             </div>
             <div class="tab-content-column-4">
-            <button id="button1-${i}" onclick="getOverlayData('${currentDisplayUserId}')">more info</button>
-            <button id="button2-${i}" onclick="closeElement('user-display-${currentDisplayUserId}')">X</button>
-            <div id="favorite-v1-${i}" onclick="setFavorite('favorite-v1-${i}','favorite-v2-${i}')">
+            <button id="button1-${userId}" onclick="openUserOverlay('${userId}')">more info</button>
+            <button id="button2-${userId}" onclick="closeElement('user-display-${userId}')">X</button>
+            <div id="favorite-v1-${userId}" onclick="setFavorite('favorite-v1-${userId}','favorite-v2-${userId}')">
             <img class="favorite-icon" src="Content/Images/favorite-v1.png">
             </div>
-            <div id="favorite-v2-${i}" style="display: none" onclick="setFavorite('favorite-v2-${i}','favorite-v1-${i}')">
+            <div id="favorite-v2-${userId}" style="display: none" onclick="setFavorite('favorite-v2-${userId}','favorite-v1-${userId}')">
             <img class="favorite-icon" src="Content/Images/favorite-v2.png">
             </div>
             </div>
             </div>`;
-        }
-    }).fail(function(reason) {
-        console.log(reason);
-    });
+
+    return userDisplay;
 }
 
 //displays the current overlay and the overlay-background
-function getOverlayData (currentOverlayUserId) {
+function openUserOverlay (currentOverlayUserId) {
     FYSCloud.API.queryDatabase(
         "SELECT * FROM user WHERE userId = ?", [currentOverlayUserId]
     ).done(function(data) {
