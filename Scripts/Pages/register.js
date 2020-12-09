ae98b77e539e7749e2a325041d0acc1790aab5ed
kiet van wijk <kiet.van.wijk@hva.nl>
2020-12-09 17:03:27 +0100

    Insert query for interests added and inserting default values for settings table

--- Scripts/Pages/register.js
+++ Scripts/Pages/register.js
@@ -192,7 +192,9 @@ let dob
 let dobFormat
 
 let bio
-let hobby
+let hobby = []
+
+let setId;
 
 // Get the current year month and date and put it inside of maximum and minimum attributes of DoB input
 const MIN_AGE = 18
@@ -286,8 +288,16 @@ function swapStep(number) {
         // Step 3 interest - No validation needed
         if (currentStep === 2) {
             bio = $('#bio').val()
-            // TODO Get value from all selected boxes
-            hobby = "test"
+
+            let hobbies = $('.hobby')
+            // Loops through all the checked hobbies and add them to the array
+            let counter = 0
+            for (let i = 0; i < hobbies.length; i++) {
+                if (hobbies[i].checked) {
+                    hobby[counter] = hobbies[i].value
+                    counter++
+                }
+            }
 
             backBtn.css('display', 'inline')
             nextBtn.css('display', 'none')
@@ -360,20 +370,28 @@ function imageUpload(photoId) {
 
 function register() {
     FYSCloud.API.queryDatabase(
-        "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES (NULL, ?, ?, ?)",
+        "INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES (NULL, ?, ?, ?);",
         [username, email, password]
     ).done(function (data) {
-        let photoId = data.insertId
-        imageUpload(photoId)
+        setId = data.insertId
+        imageUpload(setId)
 
         FYSCloud.API.queryDatabase(
-            "INSERT INTO `profile` (`id`, `userId`, `firstname`, `lastname`, `gender`, `dob`, `biography`, `pictureUrl`, `locationId`, `phone`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NULL, NULL)",
-            [data.insertId, data.insertId, firstname, lastname, gender, dobFormat, bio, "pp-" + data.insertId + ".jpg"]
+            "INSERT INTO `profile` (`id`, `userId`, `firstname`, `lastname`, `gender`, `dob`, `biography`, `pictureUrl`, `locationId`, `phone`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NULL, NULL);INSERT INTO setting (id, userId, languageId, profileVisibilityId, displayGenderId, notifcationId, maxDistance, radialDistance) VALUES (?, ?, '1', '1', '1', '1', '11', '500')",
+            [setId, setId, firstname, lastname, gender, dobFormat, bio, "pp-" + setId + ".jpg", setId, setId]
         ).done(function (data) {
-            loginUser(data.insertId)
+            for (let i = 0; i < hobby.length; i++) {
+                FYSCloud.API.queryDatabase(
+                    "INSERT INTO `userinterest` (`userId`, `interestId`) VALUES (?, ?)",
+                    [setId, hobby[i]]
+                ).fail(function (reason) {
+                    console.log(reason)
+                })
+            }
         }).fail(function (reason) {
             console.log(reason)
         })
+        loginUser(setId)
     }).fail(function (reason) {
         console.log(reason)
     })
