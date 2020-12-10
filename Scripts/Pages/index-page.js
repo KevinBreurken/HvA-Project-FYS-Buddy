f503538cd444eea46c88c37b5b9cbb455e5a709f
Kevin Breurken <kevinbreurken@gmail.com>
2020-12-10 20:34:30 +0100

    made changes to the regex and send notification when email isn't found

--- Scripts/Pages/index-page.js
+++ Scripts/Pages/index-page.js
@@ -16,11 +16,9 @@ else {
 if(this.value.match(regx)){ // little check if user has @ in email input field
     monkeyVal.classList.remove("invalid");
     monkeyVal.classList.add("valid");
-    console.log('im valid');
 }else{
     monkeyVal.classList.remove("valid");
     monkeyVal.classList.add("invalid");
-    console.log('im invalid');
 }
 });
 
@@ -28,27 +26,29 @@ function validationCheck() {
     let emailInput = document.getElementById("e-mail").value;
     let passwordInput = document.getElementById("password").value;
     //mail format checker
-    var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/g;
 
     if(emailInput == "" || passwordInput == ""){ // check if every input has been filled in
         alert('please fill in your email and password');
         return false;
     }else{
         if(emailInput.match(mailformat)){ // format check on email
-            alert("succes");
-            document.login-form.e-mail.focus();
             FYSCloud.API.queryDatabase(
                 "SELECT * FROM user WHERE `email` = ?",
                 [emailInput]
             ).done(function(data) {
-                for (let i = 0; i < data.length; i++) {
-                    if(emailInput == data[i].email && passwordInput == data[i].password) {
-                        loginUser(data[i].id);
-                        console.log(data[i].username + ' is logged in!')
-                    } else {
-                        alert("Email or password are incorrect");
-                        document.login-form.e-mail.focus();
-                        return false;
+                if(data.length == 0)
+                    alert("No e-mail found");
+                else {
+                    for (let i = 0; i < data.length; i++) {
+                        if (emailInput == data[i].email && passwordInput == data[i].password) {
+                            loginUser(data[i].id);
+                            console.log(data[i].username + ' is logged in!')
+                        } else {
+                            alert("Email or password are incorrect");
+                            document.login - form.e - mail.focus();
+                            return false;
+                        }
                     }
                 }
             })
