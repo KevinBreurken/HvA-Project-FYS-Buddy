939b57a6b50dda20b6ab1c3bd53e87c4405b0a31
kiet van wijk <kiet.van.wijk@hva.nl>
2021-01-05 14:08:51 +0100

    on boarding update

--- Scripts/on-boarding.js
+++ Scripts/on-boarding.js
@@ -1,20 +1,40 @@
-$(function () {
-    $.get("Views/on-boarding.html", function (data) {
-        $("div#on-boarding").append($(data))
-    })
-})
+// $(function () {
+//     $.get("Views/on-boarding.html", function (data) {
+//         $("div#on-boarding").append($(data))
+//     })
+// })
 
-let overlay = document.getElementById('on-boarding')
-overlay.style.display = 'none'
+let MIN_PROCEDURE = 0
+let MAX_PROCEDURE = 1
 
-function openOverlay() {
-    if (overlay.style.display === 'none') {
-        document.getElementById('on-boarding').style.display = 'block'
+let onBoarding = $('#on-boarding')
+let overlayBackground = $('#overlay-background')
+let onBoardingActive = true
+
+let procedure = $('.procedure')
+let currentProcedure = 0
+procedure[currentProcedure].style.display = 'block'
+
+// TODO remove this when live
+onBoarding.show()
+overlayBackground.show()
+
+function toggleOnBoarding() {
+    if (onBoardingActive === false) {
+        onBoarding.show()
+        overlayBackground.show()
+        onBoardingActive = true
     } else {
-        document.getElementById('on-boarding').style.display = 'none'
+        onBoarding.hide()
+        overlayBackground.hide()
+        onBoardingActive = false
     }
 }
 
-function closeOverlay(){
-    document.getElementById("on-boarding").style.display = "none"
+function swapStep(i) {
+    if (currentProcedure + i >= MIN_PROCEDURE && currentProcedure + i <= MAX_PROCEDURE) {
+        procedure[currentProcedure].style.display = 'none'
+        procedure[currentProcedure + i].style.display = 'block'
+        currentProcedure = currentProcedure + i
+    }
 }
