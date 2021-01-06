03e9abf6d7957cfa2ad2394d9052df6ba82e8d92
kiet van wijk <kiet.van.wijk@hva.nl>
2021-01-06 14:46:29 +0100

    Further improvements to on boarding

--- Scripts/on-boarding.js
+++ Scripts/on-boarding.js
@@ -1,25 +1,26 @@
-// $(function () {
-//     $.get("Views/on-boarding.html", function (data) {
-//         $("div#on-boarding").append($(data))
-//     })
-// })
-
 let MIN_PROCEDURE = 0
-let MAX_PROCEDURE = 1
+let MAX_PROCEDURE = 2
 
 let onBoarding = $('#on-boarding')
 let overlayBackground = $('#overlay-background')
-let onBoardingActive = true
+let onBoardingActive = false
+let btn = $(".btn-OB")
 
 let procedure = $('.procedure')
 let currentProcedure = 0
-procedure[currentProcedure].style.display = 'block'
-
-// TODO remove this when live
-onBoarding.show()
-overlayBackground.show()
 
 function toggleOnBoarding() {
+    // Makes it so that everyone you open open the on boarding window it starts at the first procedure
+    currentProcedure = 0
+    btn[0].style.display = 'none'
+    btn[1].style.display = 'block'
+    btn[2].style.display = 'block'
+
+    procedure[0].style.display = 'block'
+    procedure[1].style.display = 'none'
+    procedure[2].style.display = 'none'
+
+    // Shows and hides the on boarding window
     if (onBoardingActive === false) {
         onBoarding.show()
         overlayBackground.show()
@@ -32,9 +33,26 @@ function toggleOnBoarding() {
 }
 
 function swapStep(i) {
+    // Goes either the the next step or back depending on what button the user presses
     if (currentProcedure + i >= MIN_PROCEDURE && currentProcedure + i <= MAX_PROCEDURE) {
         procedure[currentProcedure].style.display = 'none'
         procedure[currentProcedure + i].style.display = 'block'
         currentProcedure = currentProcedure + i
+    }
+    // Makes sure the correct buttons are displayed on each procedure
+    if (currentProcedure === 0) {
+        btn[0].style.display = 'none'
+        btn[1].style.display = 'block'
+        btn[2].style.display = 'none'
+    }
+    if (currentProcedure === 1) {
+        btn[0].style.display = 'none'
+        btn[1].style.display = 'block'
+        btn[2].style.display = 'block'
+    }
+    if (currentProcedure === 2) {
+        btn[0].style.display = 'block'
+        btn[1].style.display = 'none'
+        btn[2].style.display = 'block'
     }
 }
