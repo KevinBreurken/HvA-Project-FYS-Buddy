43b723e9007f0617d091cc359ac9313c48dfef04
Kevin Breurken <kevinbreurken@gmail.com>
2021-01-06 20:07:50 +0100

    added fix for footer language switch
    fixed issue where footer wanted to change the setting while not logged in

--- Scripts/footer.js
+++ Scripts/footer.js
@@ -8,8 +8,16 @@ document.getElementById("english-language").onclick = function () {
 
 
 async function setLanguageSetting(languageKey, userId) {
+
+    if (userId === undefined) { //when the user is not logged in
+        FYSCloud.Localization.CustomTranslations.setLanguage(languageKey);
+        return;
+    }
+
     //Retrieve data of users settings.
-    const currentUserSetting = await getDataByPromise(`SELECT * FROM setting WHERE userId = ?;`,
+    const currentUserSetting = await getDataByPromise(`SELECT *
+                                                       FROM setting
+                                                       WHERE userId = ?;`,
         [userId]);
     //Retrieve data of given language type.
     const currentLanguage = await getDataByPromise(`SELECT *
@@ -20,12 +28,12 @@ async function setLanguageSetting(languageKey, userId) {
     //Does this user have settings?
     if (currentUserSetting.length > 0) {
         await getDataByPromise(`UPDATE setting
-                          SET languageId = ?
-                          WHERE setting.userId = ?`,
+                                SET languageId = ?
+                                WHERE setting.userId = ?`,
             [currentLanguage[0].id, userId]);
     } else {
         //Create new settings.
-        await generateDefaultSetting(userId,currentLanguage[0].id);
+        await generateDefaultSetting(userId, currentLanguage[0].id);
     }
 
     //Set the LocalStorage language.
