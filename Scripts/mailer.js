99c89d61023f3ed296858d85c37f448faa8e8029
Kevin Breurken <kevinbreurken@gmail.com>
2021-01-07 19:18:39 +0100

    added title translation to request email

--- Scripts/mailer.js
+++ Scripts/mailer.js
@@ -1,6 +1,6 @@
 FYSCloud.Localization.CustomTranslations.loadJSONTranslationFile("Content/Translations/mailer-translation.json")
 
-function sendMail(email,mailName,subject,html){
+function sendMail(email, mailName, subject, html) {
     FYSCloud.API.sendEmail({
         from: {
             name: "Find your travel Buddy - Corendon IS111-1",
@@ -17,26 +17,36 @@ function sendMail(email,mailName,subject,html){
     });
 }
 
-async function sendFriendEmail(idOfRecipient, textPrefix){
+async function sendFriendEmail(idOfRecipient, textPrefix) {
     //Check if this user has any settings
-    const recipientSetting = await getDataByPromise(`SELECT * FROM setting WHERE userId = ?`,idOfRecipient);
-    if(recipientSetting.length === 0)
+    const recipientSetting = await getDataByPromise(`SELECT *
+                                                     FROM setting
+                                                     WHERE userId = ?`, idOfRecipient);
+    if (recipientSetting.length === 0)
         return;
     //Check if the user wants any notifications
-    if(recipientSetting[0].notifcationId === 0)
+    if (recipientSetting[0].notifcationId === 0)
         return;
     //Retrieve the language key
-    const userLanguage = await getDataByPromise(`SELECT * FROM language WHERE id = ?`,recipientSetting[0].languageId);
+    const userLanguage = await getDataByPromise(`SELECT *
+                                                 FROM language
+                                                 WHERE id = ?`, recipientSetting[0].languageId);
 
     const url = window.location.href;
-    getDataByPromise(`SELECT * FROM user WHERE id = ?`,idOfRecipient).then((userData) => {
-        getDataByPromise(`SELECT * FROM profile WHERE userId = ?`,idOfRecipient).then((profileData) => {
-            const body = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.text`,userLanguage[0].languageKey);
-            const link = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.link`,userLanguage[0].languageKey);
-            const subject = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.subject`,userLanguage[0].languageKey);
-            sendMail(userData[0].email,profileData[0].firstname,subject,
-                `<h1>Hello ${profileData[0].firstname} </h1> <p>${body}</p> <a href='${url}'><p>${link}</p></a>`);
-
+    getDataByPromise(`SELECT *
+                      FROM user
+                      WHERE id = ?`, idOfRecipient).then((userData) => {
+        getDataByPromise(`SELECT *
+                          FROM profile
+                          WHERE userId = ?`, idOfRecipient).then((profileData) => {
+            const userName = profileData[0].firstname;
+            const title = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.title`, userLanguage[0].languageKey).replace('%name', userName);
+            const body = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.text`, userLanguage[0].languageKey);
+            const link = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.link`, userLanguage[0].languageKey);
+            const subject = FYSCloud.Localization.CustomTranslations.getStringFromTranslations(`mail.${textPrefix}.subject`, userLanguage[0].languageKey);
+            sendMail(userData[0].email, userName, subject,
+                `<h1>${title}</h1> <p>${body}</p> <a href='${url}'><p>${link}</p></a>`);
+            console.log("asd");
         });
     });
 }
