03ce607e467ef66212893c7d71617f45bacec603
Dylan <dylan.van.berg3@hva.nl>
2021-01-07 14:40:49 +0100

    added changepassword and made recovery mail work with userId in link

--- Scripts/Pages/recovery.js
+++ Scripts/Pages/recovery.js
@@ -1,10 +1,16 @@
+function redirectBack() {
+    location.href='index.html';
+}
+
 function emailValidation() {
-    var emailInput = document.getElementById("recovery-email").value;
+    let emailInput = document.getElementById("recovery-email").value;
     FYSCloud.API.queryDatabase(
         "SELECT * FROM user WHERE email = ?",
         [emailInput]
     ).done(function(data) {
+        console.log(data.length)
         if(data.length > 0){
+            console.log(data);
             for (let i = 0; i < data.length; i++) {
                 FYSCloud.API.sendEmail({
                     from: {
@@ -18,15 +24,16 @@ function emailValidation() {
                         }
                     ],
                     subject: "password recovery " + data[i].email,
-                    html: "<h1>Hello User </h1> <p>please click on the following link to reset your password</p> <a href='https://is111-1.fys.cloud/recovery.html'><p>Password recovery link</p></a>"
+                    html: `<h1>Hello User </h1> <p>please click on the following link to reset your password</p> <a href="https://is111-1.fys.cloud/recovery.html?id=${data[i].id}"><p>Password recovery link</p></a>`
                     }).done(function(data) {
                         alert("recovery mail has been send");
                     }).fail(function(reason) {
                         console.log(reason);
                     });
                 }
-        }
+        }else{
         alert("No email found.\nPlease make sure you are using the right e-mail address");
         $('#recovery-email').focus();
+        }
     });
 }
