b9ff5653977ffb9bfc4c7848a9676b229e541005
Kevin Breurken <kevinbreurken@gmail.com>
2021-01-13 02:43:37 +0100

    refactored code of mailer

--- Scripts/mailer.js
+++ Scripts/mailer.js
@@ -3,7 +3,7 @@ CustomTranslation.loadJSONTranslationFile("Content/Translations/mailer-translati
 /*
 Sends an email to the FYSCloud API with an Promise.
  */
-function sendEmailByPromise(email, mailName,subject,html) {
+function sendEmailByPromise(email, mailName, subject, html) {
     return new Promise((resolve, reject) => {
         FYSCloud.API.sendEmail({
             from: {
@@ -22,14 +22,15 @@ function sendEmailByPromise(email, mailName,subject,html) {
             .fail(reason => reject(reason));
     });
 }
+
 /*
 Creates an HTML string to send with an e-mail.
  */
-async function generateMailHTML(name,textPrefix,linkUrl,languageKey){
+async function generateMailHTML(name, textPrefix, linkUrl, languageKey) {
     const title = CustomTranslation.getStringFromTranslations(`mail.${textPrefix}.title`, languageKey).replace('%name', name);
     const body = CustomTranslation.getStringFromTranslations(`mail.${textPrefix}.text`, languageKey);
     const link = CustomTranslation.getStringFromTranslations(`mail.${textPrefix}.link`, languageKey);
+    const footerImageHTML = '<img src="http://drive.google.com/uc?export=view&id=1kMlZl0nUAPt2jxgH2jeG2hWGiKL_X3Cp" width="400px">';
 
-    return `<h1>${title}</h1> <p>${body}</p> <a href='${linkUrl}'><p>${link}</p></a>
-    <img src="http://drive.google.com/uc?export=view&id=1kMlZl0nUAPt2jxgH2jeG2hWGiKL_X3Cp" width="400px">`;
+    return `<h1>${title}</h1> <p>${body}</p> <a href='${linkUrl}'><p>${link}</p></a>${footerImageHTML}`;
 }
