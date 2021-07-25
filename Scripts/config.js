20be38fc1b8416cb2b7997a794a966e1e587ee9a
Kevin Breurken <kevinbreurken@hotmail.com>
2021-07-25 15:24:10 +0200

    Delete config.js

--- Scripts/config.js
+++
@@ -1,8 +0,0 @@
-const environment = window.location.origin;
-
-FYSCloud.API.configure({
-    url: "https://api.fys.cloud",
-    apiKey: "fys_is111_1.14Sh6xypTzeSUHD4",
-    database: "fys_is111_1_live", //Change this to the database-name you want to use for testing.
-    environment: "live"
-});
